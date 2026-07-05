1ce40f5201c56b08b48ef9ea626baea0e4e66419
Eki <ichinoseeki@outlook.com>
2026-07-05 20:20:50 +0800

    feat: DO NOT open browser automatically.

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-vue"],
+  webExt: {
+    disabled: true,
+  },
   manifest: {
     web_accessible_resources: [
       {
